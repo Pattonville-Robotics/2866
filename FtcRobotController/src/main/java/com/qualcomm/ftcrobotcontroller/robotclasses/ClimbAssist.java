4c9594635a4b45fab98920bb91c1504b3c3f050a
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-10-19 21:31:59 -0500

    Creation of ClimbAssist class, quick edits to Drive class
    Former-commit-id: a10bf9b4ab3903874cc44d105c5befe99beaf82c

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/robotclasses/ClimbAssist.java
@@ -0,0 +1,53 @@
+package com.qualcomm.ftcrobotcontroller.robotclasses;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Nathan Skelton on 10/19/15.
+ */
+public class ClimbAssist {
+
+    private HardwareMap hardwareMap;
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private DcMotor motorChain;
+
+    public ClimbAssist(HardwareMap hardwareMap) {
+
+        this.hardwareMap = hardwareMap;
+        this.motorLeft = hardwareMap.dcMotor.get("motor_lift_left");
+        this.motorRight = hardwareMap.dcMotor.get("motor_lift_right");
+        this.motorChain = hardwareMap.dcMotor.get("motor_chain");
+
+        motorRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void moveLift(double power) {
+
+        motorLeft.setPower(power);
+        motorRight.setPower(power);
+    }
+
+    public void moveChain(double power) {
+
+        motorChain.setPower(power);
+    }
+
+    public void stopLift() {
+
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+    }
+
+    public void stopChain() {
+
+        motorChain.setPower(0);
+    }
+
+    @Override
+    public String toString() {
+
+        return "Lift Motors: " + motorLeft.getPower() + "\n" + "Chain Motor: " + motorChain.getPower();
+    }
+}
