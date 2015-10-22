4e7ae669ff0f4d26a0a21875885b6ef117ef1f0a
Kanye West For President <jpmcmahon115@icloud.com>
2015-10-22 17:55:58 -0500

    Created ArmController

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ArmController.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ArmController.java
@@ -3,12 +3,30 @@ package org.pattonvillerobotics.team2866.robotclasses;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+
 /**
  * Created by James MacMahon on 10/20/15.
  */
 public class ArmController {
 
     private HardwareMap hardwareMap;
-    private DcMotor motorChain;
+    private DcMotor motorArm;
+
+    public ArmController(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+        this.motorArm = hardwareMap.dcMotor.get("motor_arm");
+    }
+
+    public void moveArm(double power) {
+        motorArm.setPower(power);
+    }
+
+    public void moveArmBackwards(double power) {
+        motorArm.setPower(-power);
+    }
+
+    public void stopChain() {
+        motorArm.setPower(0);
+    }
 
 }
