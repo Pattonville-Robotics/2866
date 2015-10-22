6678ec14c817995f5c0bf678148ae39515f3499a
magneticflux- <skaggsm333@gmail.com>
2015-10-22 18:44:31 -0500

    Added ExampleAutonomous class for reference. Everything possible will be done once in it. It will not actually run on a robot however.
    Former-commit-id: 829f638e3d8319959ef45be50c06e54267389d20

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/ExampleAutonomous.java
@@ -0,0 +1,28 @@
+package org.pattonvillerobotics.team2866.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by skaggsm on 10/22/15.
+ * <p/>
+ * This class is designed to teach how to use each part of the robot.
+ */
+public class ExampleAutonomous extends LinearOpMode {
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+		Servo servo1 = hardwareMap.servo.get("servo_1");
+		DcMotor motor1 = hardwareMap.dcMotor.get("motor_1");
+		DcMotorController motorController1 = hardwareMap.dcMotorController.get("motor_controller_1");
+		GyroSensor gyroSensor = hardwareMap.gyroSensor.get("gyro_1");
+
+		motor1.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+		motor1.setTargetPosition(1440);
+		motor1.getChannelMode().equals(DcMotorController.RunMode.RUN_TO_POSITION);
+	}
+}
