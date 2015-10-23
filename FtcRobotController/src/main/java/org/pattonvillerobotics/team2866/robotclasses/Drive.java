d064baaf658c774dc2cce8eef33b7219fa7fa75d
magneticflux- <skaggsm333@gmail.com>
2015-10-22 20:05:27 -0500

    Added some preliminary encoder methods in the Drive class.
    Former-commit-id: 4bce75b79257c39473abfb6a4cf43538258fd824

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Drive.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Drive.java
@@ -2,15 +2,19 @@ package org.pattonvillerobotics.team2866.robotclasses;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Nathan Skelton on 10/15/15.
  */
 public class Drive {
 
+	public static final double WHEEL_RADIUS = 10;
+
 	private HardwareMap hardwareMap;
 	private DcMotor motorLeft;
 	private DcMotor motorRight;
+	private Servo servoClimber;
 
 
     public Drive(HardwareMap hardwareMap) {
@@ -18,8 +22,13 @@ public class Drive {
 		this.hardwareMap = hardwareMap;
 		this.motorLeft = hardwareMap.dcMotor.get("motor_drive_left");
 		this.motorRight = hardwareMap.dcMotor.get("motor_drive_right");
+	    this.servoClimber = hardwareMap.servo.get("servo_climber");
 
-        motorRight.setDirection(DcMotor.Direction.REVERSE);
+	    motorRight.setDirection(DcMotor.Direction.REVERSE);
+	}
+
+	public static double cmToTicks(double cm) {
+		return 0;
 	}
 
 	public void moveFreely(double left, double right) {
@@ -28,10 +37,10 @@ public class Drive {
 		motorRight.setPower(right);
 	}
 
-	public void moveStraight(double power){
+	public void moveStraight(double power) {
 
 		motorRight.setPower(power);
-        motorLeft.setPower(power);
+		motorLeft.setPower(power);
 	}
 
 	public void rotateLeft(double power) {
@@ -60,9 +69,9 @@ public class Drive {
 
     }
 
-    @Override
-    public String toString() {
+	@Override
+	public String toString() {
 
-        return "Drive Left Motor: " + motorLeft.getPower() + "\n" + "Drive Right Power: " + motorRight.getPower();
-    }
+		return "Drive Left Motor: " + motorLeft.getPower() + "\n" + "Drive Right Power: " + motorRight.getPower();
+	}
 }
