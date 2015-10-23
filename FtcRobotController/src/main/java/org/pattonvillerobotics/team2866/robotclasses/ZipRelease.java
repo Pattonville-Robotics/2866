8184a0d6982dd4c1f23e3c4af5abfd9e00772f39
TheKrazyStew <krazystew@gmail.com>
2015-10-22 19:55:16 -0500

    Fixed Arm Control.

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by mcmahonj on 10/22/15 AD.
  */
 
+//http://massftc.org/wp-content/uploads/newTechIntro_AndroidStudio_with_PushBot.pdf
+
 public class ZipRelease {
     private HardwareMap hardwareMap;
 
