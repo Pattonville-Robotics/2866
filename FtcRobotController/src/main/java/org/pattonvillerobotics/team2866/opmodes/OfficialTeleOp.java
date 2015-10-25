de4233dfd3a793060c56a4c4d4dd7b9368d726f3
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2015-10-25 15:32:34 -0500

    Implemented Classes in the TeleOp, enums for ZipRelease
    Former-commit-id: a50336011ed74ca3da18ce02194cdf7e731f155c

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/OfficialTeleOp.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/OfficialTeleOp.java
@@ -3,7 +3,10 @@ package org.pattonvillerobotics.team2866.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.Range;
 
+import org.pattonvillerobotics.team2866.robotclasses.ArmController;
+import org.pattonvillerobotics.team2866.robotclasses.ClimbAssist;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
+import org.pattonvillerobotics.team2866.robotclasses.ZipRelease;
 
 /**
  * Created by Team 2866 on 10/6/15.
@@ -13,16 +16,30 @@ public class OfficialTeleOp extends OpMode {
     public static final String TAG = "TestOp";
 
     private Drive drive;
+    private ClimbAssist climbAssist;
+    private ArmController armController;
+    private ZipRelease zipRelease;
+
+
+    private boolean leftReleaseDown = false;
+    private boolean leftReleaseTriggered = false;
+    private boolean rightReleaseDown = false;
+    private boolean rightReleaseTriggered = false;
 
     public void init() {
 
         drive = new Drive(hardwareMap);
+        climbAssist = new ClimbAssist(hardwareMap);
+        armController = new ArmController(hardwareMap);
+        zipRelease = new ZipRelease(hardwareMap);
 
         gamepad1.setJoystickDeadzone(0.05f);
         gamepad2.setJoystickDeadzone(0.05f);
     }
 
     public void loop() {
+
+        // Treads
 
         float right = gamepad1.right_stick_y;
         float left = gamepad1.left_stick_y;
@@ -33,7 +50,71 @@ public class OfficialTeleOp extends OpMode {
 
         drive.moveFreely(left, right);
 
-        telemetry.addData(TAG, "Teleop");
+
+        // Climb Assist
+
+        if (gamepad1.y) {
+            climbAssist.moveLift(.25);
+        } else if (gamepad1.a) {
+            climbAssist.moveLift(-.25);
+        } else {
+            climbAssist.stopLift();
+        }
+
+        if (gamepad1.b) {
+            climbAssist.moveChain(.25);
+        }
+
+
+        // Arm
+
+        if (gamepad2.y) {
+            armController.moveArm(.25);
+        } else if (gamepad2.a) {
+            armController.moveArm(-.25);
+        } else {
+            armController.stopArm();
+        }
+
+
+        // Zip Release
+
+        if (gamepad2.x) {
+            if (!leftReleaseTriggered) {
+                if (leftReleaseDown) {
+                    zipRelease.moveLeft(ZipRelease.Direction.Up);
+                    leftReleaseDown = false;
+                } else {
+                    zipRelease.moveLeft(ZipRelease.Direction.Down);
+                }
+                leftReleaseTriggered = true;
+            }
+        }
+        else {
+            leftReleaseTriggered = false;
+        }
+
+        if (gamepad2.b) {
+            if (!rightReleaseTriggered) {
+                if (rightReleaseDown) {
+                    zipRelease.moveRight(ZipRelease.Direction.Up);
+                    leftReleaseDown = false;
+                } else {
+                    zipRelease.moveRight(ZipRelease.Direction.Down);
+                }
+                rightReleaseTriggered = true;
+            }
+        }
+        else {
+            rightReleaseTriggered = false;
+        }
+
+
+        // Telemetry
+
         telemetry.addData(TAG, drive);
+        telemetry.addData(TAG, climbAssist);
+        telemetry.addData(TAG, armController);
+        telemetry.addData(TAG, zipRelease);
     }
 }
--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
@@ -6,6 +6,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by James McMahon on 10/22/15.
  */
 public class ZipRelease {
-    private HardwareMap hardwareMap;
 
+    public enum Direction {
+        Up, Down
+    }
+
+    private HardwareMap hardwareMap;
 }
