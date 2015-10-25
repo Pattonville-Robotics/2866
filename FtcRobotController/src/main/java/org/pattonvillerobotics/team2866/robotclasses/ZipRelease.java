6b139f4ceb78e72ff4ae4eea82394d3a7de4a91e
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2015-10-25 14:52:25 -0500

    Comment edit

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
@@ -3,11 +3,8 @@ package org.pattonvillerobotics.team2866.robotclasses;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
- * Created by mcmahonj on 10/22/15 AD.
+ * Created by James McMahon on 10/22/15.
  */
-
-//http://massftc.org/wp-content/uploads/newTechIntro_AndroidStudio_with_PushBot.pdf
-
 public class ZipRelease {
     private HardwareMap hardwareMap;
 
