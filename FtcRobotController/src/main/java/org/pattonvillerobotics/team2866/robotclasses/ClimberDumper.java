1aca5ee1f18026e39b6fb2d4f319531dae8b28e3
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-10-25 16:52:04 -0500

    Created ClimberDumper class, added TODO's

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ClimberDumper.java
@@ -0,0 +1,22 @@
+package org.pattonvillerobotics.team2866.robotclasses;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Nathan Skelton on 10/25/15.
+ *
+ * TODO: Create Constructor
+ * TODO: Create method move() using DirectionEnum
+ * TODO: Determine servo values for up and down
+ */
+public class ClimberDumper {
+
+    private HardwareMap hardwareMap;
+
+    private Servo servo;
+
+    private static double UP = 0;
+    private static double DOWN = 0;
+
+}
