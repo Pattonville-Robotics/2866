f64cd9fa77a6d4aa560a56428625827ffcdf1084
TheKrazyStew <krazystew@gmail.com>
2015-10-27 15:23:38 -0500

    Did the todos.
    Former-commit-id: 7bfdcec233520ddd4850abfb71faf8325843f3c4

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ZipRelease.java
@@ -6,9 +6,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by James McMahon on 10/22/15.
  *
- * TODO: Create Constructor
- * TODO: Create methods moveLeft() and moveRight()
- * TODO: Determine servo values for up and down positions on left and right releases
  */
 public class ZipRelease {
 
@@ -17,9 +14,24 @@ public class ZipRelease {
     private Servo servoLeft;
     private Servo servoRight;
 
-    private static double LEFT_UP = 0;
-    private static double LEFT_DOWN = 0;
-    private static double RIGHT_UP = 0;
-    private static double RIGHT_DOWN = 0;
+    private static final double LEFT_UP = 0;
+    private static final double LEFT_DOWN = 0;
+    private static final double RIGHT_UP = 0;
+    private static final double RIGHT_DOWN = 0;
 
+
+    public ZipRelease(HardwareMap hardwareMap) {
+
+        this.hardwareMap = hardwareMap;
+    }
+
+    public void moveLeft(DirectionEnum direction) {
+        servoLeft.setPosition(LEFT_UP);
+        servoLeft.setPosition(LEFT_DOWN);
+    }
+
+    public void moveRight(DirectionEnum direction) {
+        servoRight.setPosition(RIGHT_UP);
+        servoRight.setPosition(RIGHT_DOWN);
+    }
 }
