37fe89f2287020eaead533465bf027f29f2a9739
rhinesms <nessixtyfive@gmail.com>
2015-10-27 15:02:40 -0500

    Completed ClimberDumper constructor
    Former-commit-id: f0fcbb6b40bc7fb0e5327fcfae1f4c80945369b3

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ClimberDumper.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ClimberDumper.java
@@ -5,18 +5,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Nathan Skelton on 10/25/15.
- *
- * TODO: Create Constructor
+ * <p/>
  * TODO: Create method move() using DirectionEnum
  * TODO: Determine servo values for up and down
  */
 public class ClimberDumper {
 
-    private HardwareMap hardwareMap;
-
-    private Servo servo;
-
     private static double UP = 0;
     private static double DOWN = 0;
+    private HardwareMap hardwareMap;
+    private Servo servo;
+
+    public ClimberDumper(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+        this.servo = this.hardwareMap.servo.get("servo");
+    }
+
 
 }
