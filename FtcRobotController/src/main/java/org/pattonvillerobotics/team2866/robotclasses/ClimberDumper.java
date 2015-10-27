9415ffa156f8a202d24f7171d200e64e0a60be9f
rhinesms <nessixtyfive@gmail.com>
2015-10-27 15:37:48 -0500

    Completed move() with directionenum
    Former-commit-id: 39a5202b253a92bcf09039ac0d23da18e10afae4

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ClimberDumper.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/ClimberDumper.java
@@ -6,20 +6,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by Nathan Skelton on 10/25/15.
  * <p/>
- * TODO: Create method move() using DirectionEnum
+ *
  * TODO: Determine servo values for up and down
  */
 public class ClimberDumper {
 
-    private static double UP = 0;
-    private static double DOWN = 0;
+    private static final double UP = 0;
+    private static final double DOWN = 0;
     private HardwareMap hardwareMap;
     private Servo servo;
 
     public ClimberDumper(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
         this.servo = this.hardwareMap.servo.get("servo");
+
     }
 
+    public void move(DirectionEnum direction)
 
 }
