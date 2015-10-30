413b8bb8600d21e866801a2b52c212d7745f21c0
magneticflux- <skaggsm333@gmail.com>
2015-10-30 15:06:07 -0500

    Added an awesome annotation processing system for OpModes. The FtcOpModeRegister class is no longer needed. Add the annotation to a class that extends OpMode and specify the name to register it as.

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/OpMode.java
@@ -0,0 +1,19 @@
+package org.pattonvillerobotics.team2866.robotclasses;
+
+import org.atteo.classindex.IndexAnnotated;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by skaggsm on 10/30/15.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@IndexAnnotated
+public @interface OpMode {
+
+    String name() default "Name not set.";
+}
