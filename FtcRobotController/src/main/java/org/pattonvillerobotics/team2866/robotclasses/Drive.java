76d0e85c399e26050a810c3431e67076d9e73768
magneticflux- <skaggsm333@gmail.com>
2015-10-30 17:28:02 -0500

    Added rotation methods to the drive class and removed unused TestAutonomous.
    Former-commit-id: 8b76b59aa0f136c322a4408d1e9460568f5b1635

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Drive.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Drive.java
@@ -7,13 +7,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 /**
  * Created by Nathan Skelton on 10/15/15.
  * Last edited by Mitchell Skaggs on 10/30/15
- * <p/>
- * TODO: Determine the calculations for inchesToTicks(double inches)
- * TODO Create method for rotation
  */
 public class Drive {
 
-    public static final double WHEEL_RADIUS = -1;
+    public static final double WHEEL_RADIUS = -1; // TODO Find appropriate values
+    public static final double WHEEL_CIRCUMFERENCE = 2 * Math.PI * WHEEL_RADIUS;
+    public static final double TICKS_PER_REVOLUTION = 1440;
+    public static final double INCHES_PER_TICK = WHEEL_CIRCUMFERENCE / TICKS_PER_REVOLUTION;
+
+    public static final double WHEEL_BASE_RADIUS = -1; // TODO Find appropriate values
+    public static final double WHEEL_BASE_CIRCUMFERENCE = 2 * Math.PI * WHEEL_BASE_RADIUS;
+    public static final int DEGREES_PER_REVOLUTION = 360; // Why lol
+    public static final double INCHES_PER_DEGREE = WHEEL_BASE_CIRCUMFERENCE / DEGREES_PER_REVOLUTION;
 
     private HardwareMap hardwareMap;
     private DcMotor motorLeft;
@@ -29,7 +34,11 @@ public class Drive {
     }
 
     public static double inchesToTicks(double inches) {
-        return 9001.6969;
+        return inches / INCHES_PER_TICK;
+    }
+
+    public static double degreesToTicks(double degrees) {
+        return inchesToTicks(degrees * INCHES_PER_DEGREE);
     }
 
     public void moveFreely(double left, double right) {
@@ -38,37 +47,37 @@ public class Drive {
         motorRight.setPower(right);
     }
 
+    @Deprecated
     public void moveStraight(double power) {
-
         motorRight.setPower(power);
         motorLeft.setPower(power);
     }
 
+    @Deprecated
     public void rotateLeft(double power) {
-
         motorRight.setPower(power);
         motorLeft.setPower(-power);
     }
 
+    @Deprecated
     public void rotateRight(double power) {
-
         motorRight.setPower(-power);
         motorLeft.setPower(power);
     }
 
+    @Deprecated
     public void stop() {
-
         motorLeft.setPower(0);
         motorRight.setPower(0);
     }
 
     public void moveInches(DirectionEnum direction, double inches, double power) {
         if (Math.signum(motorLeft.getCurrentPosition()) != Math.signum(motorRight.getCurrentPosition()))
-            throw new AssertionError("robt is kill");
+            throw new AssertionError("robit is kill");
         if (power > 1 || power < 0)
-            throw new IllegalArgumentException("Invalid power value");
+            throw new IllegalArgumentException("Power must be positive!");
         if (inches <= 0)
-            throw new IllegalArgumentException("Invalid distance");
+            throw new IllegalArgumentException("Distance must be positive!");
 
         int targetPosition;
 
@@ -86,7 +95,7 @@ public class Drive {
                 break;
             }
             default:
-                throw new IllegalArgumentException("Direction must be FORWARDS or BACKWARDS");
+                throw new IllegalArgumentException("Direction must be FORWARDS or BACKWARDS!");
         }
 
         motorLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
@@ -100,19 +109,52 @@ public class Drive {
     }
 
     public void rotateDegrees(DirectionEnum direction, double degrees, double power) {
+        if (degrees <= 0)
+            throw new IllegalArgumentException("Degrees must be positive!");
+        if (power <= 0)
+            throw new IllegalArgumentException("Power must be positive!");
+
+        int targetLeft;
+        int targetRight;
+
         switch (direction) {
-            case LEFT:
+            case LEFT: {
+                int startPositionLeft = motorLeft.getCurrentPosition();
+                int deltaPositionLeft = (int) Math.round(degreesToTicks(degrees));
+                targetLeft = startPositionLeft - deltaPositionLeft;
+
+                int startPositionRight = motorLeft.getCurrentPosition();
+                int deltaPositionRight = (int) Math.round(degreesToTicks(degrees));
+                targetRight = startPositionRight + deltaPositionRight;
                 break;
-            case RIGHT:
+            }
+            case RIGHT: {
+                int startPositionLeft = motorLeft.getCurrentPosition();
+                int deltaPositionLeft = (int) Math.round(degreesToTicks(degrees));
+                targetLeft = startPositionLeft + deltaPositionLeft;
+
+                int startPositionRight = motorLeft.getCurrentPosition();
+                int deltaPositionRight = (int) Math.round(degreesToTicks(degrees));
+                targetRight = startPositionRight - deltaPositionRight;
                 break;
+            }
             default:
-                throw new IllegalArgumentException("Direction must be LEFT or RIGHT");
+                throw new IllegalArgumentException("Direction must be LEFT or RIGHT!");
         }
+
+        motorLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motorRight.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        motorLeft.setTargetPosition(targetLeft);
+        motorRight.setTargetPosition(targetRight);
+
+        motorLeft.setPower(power);
+        motorRight.setPower(power);
     }
 
     @Override
     public String toString() {
 
-        return "Drive Left Motor: " + motorLeft.getPower() + "\n" + "Drive Right Power: " + motorRight.getPower();
+        return "Drive Left Power: " + motorLeft.getPower() + ", Drive Right Power: " + motorRight.getPower();
     }
 }
