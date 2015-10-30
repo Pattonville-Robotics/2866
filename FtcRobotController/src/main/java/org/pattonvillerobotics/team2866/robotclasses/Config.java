8b167d88ed7684f255101ef3c3f916973f8cfe68
magneticflux- <skaggsm333@gmail.com>
2015-10-30 17:34:52 -0500

    Added Config system for easily replaceable hardware IDs.

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
@@ -0,0 +1,20 @@
+package org.pattonvillerobotics.team2866.robotclasses;
+
+/**
+ * Created by skaggsm on 10/30/15.
+ * <p/>
+ * TODO Get names for all hardware on the robot
+ */
+
+public final class Config {
+
+    public static final String motorDriveLeft = "motor_drive_left";
+    public static final String motorDriveRight = "motor_drive_right";
+    public static final String motorArmLeft = "motor_arm_left";
+    public static final String motorArmRight = "motor_arm_right";
+
+    public static final String servoClimber = "servo_climber";
+
+    public static final String sensorGyro = "sensor_gyro";
+    public static final String sensorLight = "sensor_light";
+}
