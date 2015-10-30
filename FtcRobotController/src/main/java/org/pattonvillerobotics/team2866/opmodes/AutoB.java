a9166cebdb059a6376b6b53bf4220a2d1e213634
magneticflux- <skaggsm333@gmail.com>
2015-10-30 10:07:17 -0500

    Updated autonomouses

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
@@ -2,31 +2,33 @@ package org.pattonvillerobotics.team2866.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.pattonvillerobotics.team2866.robotclasses.DirectionEnum;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
 
 /**
  * Created by Kevin Stewart & James McMahon on 10/15/15.
  */
-public class AutoB extends LinearOpMode{
-	public static final String TAG = "TestAutonomous";
+public class AutoB extends LinearOpMode {
 
-	@Override
-	public void runOpMode() throws InterruptedException {
+    public static final String TAG = "TestAutonomous";
 
-		Drive drive = new Drive(hardwareMap);
+    @Override
+    public void runOpMode() throws InterruptedException {
 
-		waitForStart();
+        Drive drive = new Drive(hardwareMap);
 
-        drive.moveInches(30); //Make sure this doesn't cross over! Go 30 inches
-        drive.rotateRightDegrees(45); //Make sure this goes at a 45˚ angle!
-        drive.moveInches(92); //92 inches or w/e up to the rescue bit
-        drive.rotateRightDegrees(45);
-        drive.moveInches(18); //18 inches: NIN*2
-        drive.moveInches(-18);
-        drive.rotateLeftDegrees(45);
-        drive.moveInches(-18);
-        drive.retateLeftDegrees(90);
-        drive.moveInches(102); //Measurement required
+        waitForStart();
+
+        drive.moveInches(DirectionEnum.FORWARDS, 30, 1); //Make sure this doesn't cross over! Go 30 inches
+        drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1); //Make sure this goes at a 45˚ angle!
+        drive.moveInches(DirectionEnum.FORWARDS, 92, 1); //92 inches or w/e up to the rescue bit
+        drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1);
+        drive.moveInches(DirectionEnum.FORWARDS, 18, 1); //18 inches: NIN*2
+        drive.moveInches(DirectionEnum.BACKWARDS, 18, 1);
+        drive.rotateDegrees(DirectionEnum.LEFT, 45, 1);
+        drive.moveInches(DirectionEnum.BACKWARDS, -18, 1);
+        drive.rotateDegrees(DirectionEnum.LEFT, 90, 1);
+        drive.moveInches(DirectionEnum.FORWARDS, 102, 1); //Measurement required
         drive.stop();
-	}
+    }
 }
