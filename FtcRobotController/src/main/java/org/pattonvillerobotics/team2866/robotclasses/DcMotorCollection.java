f2929199998ad8c68cdc2a5a3a680e8ae28d19ca
magneticflux- <skaggsm333@gmail.com>
2015-10-30 10:06:06 -0500

    Finished DcMotorCollection class

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/DcMotorCollection.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/DcMotorCollection.java
@@ -6,14 +6,15 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import java.util.Arrays;
 import java.util.List;
 
+import java8.util.function.BinaryOperator;
+import java8.util.function.Consumer;
 import java8.util.function.Function;
+import java8.util.function.Predicate;
 import java8.util.stream.Collectors;
 import java8.util.stream.StreamSupport;
 
 /**
  * Created by skaggsm on 10/29/15.
- * <p/>
- * TODO Make multi-motor analogues for each method
  */
 public class DcMotorCollection extends DcMotor {
 
@@ -36,86 +37,171 @@ public class DcMotorCollection extends DcMotor {
 
     @Override
     public String getConnectionInfo() {
-        return super.getConnectionInfo();
+        return StreamSupport.stream(mDcMotors).map(new Function<DcMotor, String>() {
+            @Override
+            public String apply(DcMotor dcMotor) {
+                return dcMotor.getConnectionInfo();
+            }
+        }).collect(Collectors.joining(", "));
     }
 
     @Override
     public int getVersion() {
-        return super.getVersion();
+        return StreamSupport.stream(mDcMotors).map(new Function<DcMotor, Integer>() {
+            @Override
+            public Integer apply(DcMotor dcMotor) {
+                return dcMotor.getVersion();
+            }
+        }).reduce(0, new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer & integer2;
+            }
+        });
     }
 
     @Override
     public void close() {
-        super.close();
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.close();
+            }
+        });
     }
 
     @Override
     public DcMotorController getController() {
-        return super.getController();
+        return null;
     }
 
     @Override
     public Direction getDirection() {
-        return super.getDirection();
+        return null;
     }
 
     @Override
-    public void setDirection(Direction direction) {
-        super.setDirection(direction);
+    public void setDirection(final Direction direction) {
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.setDirection(direction);
+            }
+        });
     }
 
     @Override
     public int getPortNumber() {
-        return super.getPortNumber();
+        return -1;
     }
 
     @Override
     public double getPower() {
-        return super.getPower();
+        return StreamSupport.stream(mDcMotors).map(new Function<DcMotor, Double>() {
+            @Override
+            public Double apply(DcMotor dcMotor) {
+                return dcMotor.getPower();
+            }
+        }).reduce(0d, new BinaryOperator<Double>() {
+            @Override
+            public Double apply(Double d1, Double d2) {
+                return Double.longBitsToDouble(Double.doubleToLongBits(d1) & Double.doubleToLongBits(d2));
+            }
+        });
     }
 
     @Override
-    public void setPower(double power) {
-        super.setPower(power);
+    public void setPower(final double power) {
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.setPower(power);
+            }
+        });
     }
 
     @Override
     public boolean isBusy() {
-        return super.isBusy();
+        return StreamSupport.stream(mDcMotors).anyMatch(new Predicate<DcMotor>() {
+            @Override
+            public boolean test(DcMotor dcMotor) {
+                return dcMotor.isBusy();
+            }
+        });
     }
 
     @Override
     public void setPowerFloat() {
-        super.setPowerFloat();
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.setPowerFloat();
+            }
+        });
     }
 
     @Override
     public boolean getPowerFloat() {
-        return super.getPowerFloat();
+        return StreamSupport.stream(mDcMotors).anyMatch(new Predicate<DcMotor>() {
+            @Override
+            public boolean test(DcMotor dcMotor) {
+                return dcMotor.getPowerFloat();
+            }
+        });
     }
 
     @Override
     public int getTargetPosition() {
-        return super.getTargetPosition();
+        return StreamSupport.stream(mDcMotors).map(new Function<DcMotor, Integer>() {
+            @Override
+            public Integer apply(DcMotor dcMotor) {
+                return dcMotor.getTargetPosition();
+            }
+        }).reduce(0, new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer & integer2;
+            }
+        });
     }
 
     @Override
-    public void setTargetPosition(int position) {
-        super.setTargetPosition(position);
+    public void setTargetPosition(final int position) {
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.setTargetPosition(position);
+            }
+        });
     }
 
     @Override
     public int getCurrentPosition() {
-        return super.getCurrentPosition();
+        return StreamSupport.stream(mDcMotors).map(new Function<DcMotor, Integer>() {
+            @Override
+            public Integer apply(DcMotor dcMotor) {
+                return dcMotor.getCurrentPosition();
+            }
+        }).reduce(0, new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer & integer2;
+            }
+        });
     }
 
     @Override
     public DcMotorController.RunMode getChannelMode() {
-        return super.getChannelMode();
+        return null;
     }
 
     @Override
-    public void setChannelMode(DcMotorController.RunMode mode) {
-        super.setChannelMode(mode);
+    public void setChannelMode(final DcMotorController.RunMode mode) {
+        StreamSupport.stream(mDcMotors).forEach(new Consumer<DcMotor>() {
+            @Override
+            public void accept(DcMotor dcMotor) {
+                dcMotor.setChannelMode(mode);
+            }
+        });
     }
 }
