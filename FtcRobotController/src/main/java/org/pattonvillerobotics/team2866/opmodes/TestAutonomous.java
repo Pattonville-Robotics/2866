2c995ae99d20b843938ea61b03a660745ecdd346
magneticflux- <skaggsm333@gmail.com>
2015-11-10 16:58:27 -0600

    More work on the Test Autonomous
    Former-commit-id: 64fe63488d6efc775ee8c322144c3b82c18202c9

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
@@ -1,8 +1,8 @@
 package org.pattonvillerobotics.team2866.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
-import org.pattonvillerobotics.team2866.robotclasses.DirectionEnum;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
 import org.pattonvillerobotics.team2866.robotclasses.OpMode;
 
@@ -20,6 +20,41 @@ public class TestAutonomous extends LinearOpMode {
         Drive drive = new Drive(hardwareMap, this);
         waitForStart();
 
-        drive.moveInches(DirectionEnum.FORWARDS, 30, 1);
+        telemetry.addData("FLOW", "Starting move");
+
+        int leftStart = drive.motorLeft.getCurrentPosition();
+        int rightStart = drive.motorLeft.getCurrentPosition();
+
+        waitOneFullHardwareCycle();
+
+        drive.motorLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        drive.motorRight.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        waitOneFullHardwareCycle();
+
+        drive.motorLeft.setTargetPosition(leftStart + 1440 * 4);
+        drive.motorRight.setTargetPosition(rightStart + 1440 * 4);
+
+        waitOneFullHardwareCycle();
+
+        drive.motorLeft.setPower(1);
+        drive.motorRight.setPower(1);
+
+        waitOneFullHardwareCycle();
+
+        for (int i = 0; i < 10; i++) {
+            sleep(200);
+            waitForNextHardwareCycle();
+            telemetry.addData("LMotor", "Location: " + drive.motorLeft.getCurrentPosition());
+            telemetry.addData("RMotor", "Location: " + drive.motorRight.getCurrentPosition());
+        }
+
+        waitOneFullHardwareCycle();
+
+        drive.motorLeft.setPower(0);
+        drive.motorRight.setPower(0);
+
+        //drive.moveInches(DirectionEnum.FORWARDS, 30, 1);
+        telemetry.addData("FLOW", "Finished move");
     }
 }
