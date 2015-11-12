9b16b1ae83dc73df0b40197e2245a8eb36b4a6ea
magneticflux- <skaggsm333@gmail.com>
2015-11-12 07:15:44 -0600

    Increased sampling max time
    Former-commit-id: 3d606de8126e787243d331cdf503bf26832fc4df

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
@@ -42,7 +42,7 @@ public class TestAutonomous extends LinearOpMode {
 
         waitOneFullHardwareCycle();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             sleep(200);
             waitForNextHardwareCycle();
             telemetry.addData("LMotor", "Location: " + drive.motorLeft.getCurrentPosition());
