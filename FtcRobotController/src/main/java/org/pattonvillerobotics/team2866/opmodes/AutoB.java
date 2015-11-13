62d2a32fe36d781b90858f14331cb2b61abada3c
TheKrazyStew <krazystew@gmail.com>
2015-11-12 18:11:13 -0600

    Fixed distances
    Former-commit-id: c3f9878c4f142237afde759bd78a702a42ab2094

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
@@ -27,7 +27,7 @@ public class AutoB extends LinearOpMode {
 
         drive.moveInches(DirectionEnum.BACKWARDS, 30, 1); //Make sure this doesn't cross over! Go 30 inches
         drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1); //Make sure this goes at a 45˚ angle!
-        drive.moveInches(DirectionEnum.BACKWARDS, 92, 1); //92 inches or w/e up to the rescue bit
+        drive.moveInches(DirectionEnum.BACKWARDS, 72, 1); //92 inches or w/e up to the rescue bit
         drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1);
         drive.moveInches(DirectionEnum.BACKWARDS, 18, 1); //18 inches: NIN*2
         climberDumper.move(DirectionEnum.UP); //Moves the servo to dump the lil guys into the basket
@@ -37,7 +37,7 @@ public class AutoB extends LinearOpMode {
         drive.moveInches(DirectionEnum.FORWARDS, 18, 1);
         drive.rotateDegrees(DirectionEnum.LEFT, 90, 1);
         climbAssist.moveChain(1);
-        drive.moveInches(DirectionEnum.FORWARDS, 102, 1); //Measurement required
+        drive.moveInches(DirectionEnum.FORWARDS, 60, 1); //Measurement required
         climbAssist.moveChain(0);
         drive.stop();
     }
