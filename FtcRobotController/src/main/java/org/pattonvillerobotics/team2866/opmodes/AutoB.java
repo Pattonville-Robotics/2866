e9af1f84b323e9481eb76734d5fd1cd003226e19
TheKrazyStew <krazystew@gmail.com>
2015-11-12 19:30:50 -0600

    Adjusted measurements and direction using human reenactment.

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoB.java
@@ -26,19 +26,20 @@ public class AutoB extends LinearOpMode {
         waitForStart();
 
         drive.moveInches(DirectionEnum.BACKWARDS, 30, 1); //66
-        drive.rotateDegrees(DirectionEnum.LEFT, 135, 1); //Make sure this goes at a 45˚ angle!
+        drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1); //Make sure this goes at a 45˚ angle!
         drive.moveInches(DirectionEnum.BACKWARDS, 72, 1); //92 inches or w/e up to the rescue bit
         drive.rotateDegrees(DirectionEnum.RIGHT, 45, 1);
         drive.moveInches(DirectionEnum.BACKWARDS, 18, 1); //18 inches: NIN*2
         climberDumper.move(DirectionEnum.UP); //Moves the servo to dump the lil guys into the basket
+        wait(1000);
         climberDumper.move(DirectionEnum.DOWN);
         drive.moveInches(DirectionEnum.FORWARDS, 18, 1);
         drive.rotateDegrees(DirectionEnum.LEFT, 45, 1);
         drive.moveInches(DirectionEnum.FORWARDS, 18, 1);
         drive.rotateDegrees(DirectionEnum.LEFT, 90, 1);
-        climbAssist.moveChain(1);
         drive.moveInches(DirectionEnum.FORWARDS, 60, 1); //Measurement required
-        climbAssist.moveChain(0);
+        //climbAssist.moveChain(1);
+        //climbAssist.moveChain(0);
         drive.stop();
     }
 }
