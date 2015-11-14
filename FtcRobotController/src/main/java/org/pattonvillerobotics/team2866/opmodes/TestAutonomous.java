508f6a0f396d31e909619d87a5c648ea510f8397
magneticflux- <skaggsm333@gmail.com>
2015-11-14 09:38:35 -0600

    Added logging to the test autonomous.

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/TestAutonomous.java
@@ -1,7 +1,6 @@
 package org.pattonvillerobotics.team2866.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 
 import org.pattonvillerobotics.team2866.robotclasses.DirectionEnum;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
@@ -15,17 +14,22 @@ public class TestAutonomous extends LinearOpMode {
 
     public static final String TAG = "TestAutonomous";
 
+    @SuppressWarnings("MagicNumber")
     @Override
     public void runOpMode() throws InterruptedException {
 
         Drive drive = new Drive(hardwareMap, this);
         waitForStart();
 
-        drive.moveInches(DirectionEnum.FORWARDS,30,.5);
-        drive.moveInches(DirectionEnum.BACKWARDS,30,.5);
+        telemetry.addData("FLOW", "Started move");
+        telemetry.addData("FLOW", "Moving forwards");
+        drive.moveInches(DirectionEnum.FORWARDS, 30, .75);
+        telemetry.addData("FLOW", "Moving backwards");
+        drive.moveInches(DirectionEnum.BACKWARDS, 30, .75);
 
-        drive.rotateDegrees(DirectionEnum.LEFT,90,.5);
-        drive.rotateDegrees(DirectionEnum.RIGHT,90,.5);
+        //drive.rotateDegrees(DirectionEnum.LEFT, 90, 1);
+        telemetry.addData("FLOW", "Rotating right");
+        drive.rotateDegrees(DirectionEnum.RIGHT, 90, .75);
 
         //drive.moveInches(DirectionEnum.FORWARDS, 30, 1);
         telemetry.addData("FLOW", "Finished move");
