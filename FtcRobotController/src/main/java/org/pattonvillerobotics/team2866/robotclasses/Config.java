d5b57668468a0c64cbd6073b51ae89e1492f3499
Mitchell Skaggs <skaggsm333@gmail.com>
2015-11-14 16:26:31 -0600

    Added a rotate method that uses the Modern Robotics Gyroscope. It assumes that the integrated Z value is the angular displacement, not constrained to 0 - 359.

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
@@ -28,5 +28,5 @@ public final class Config {
     public static final double CHAIN_MOVEMENT_SPEED = .25;
     public static final double LIFT_MOVEMENT_SPEED = .25;
     public static final int ENCODER_MOVEMENT_TOLERANCE = 24;
-    public static final int ENCODER_MOVEMENT_UPDATE_DELAY = -1;
+    public static final int GYRO_TURN_TOLERANCE = 2;
 }
