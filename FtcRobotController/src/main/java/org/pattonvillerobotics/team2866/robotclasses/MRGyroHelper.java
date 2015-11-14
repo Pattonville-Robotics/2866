e4c063464bd8374ac4fbef7319ae5ae7d7d7bfef
magneticflux- <skaggsm333@gmail.com>
2015-11-14 13:41:55 -0600

    Added Gyro logging, a new Gyro class, and a new rotate method.
    Former-commit-id: a58bbce09b1398fbf85c4e3188db94ad94d8bd76

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/MRGyroHelper.java
@@ -0,0 +1,32 @@
+package org.pattonvillerobotics.team2866.robotclasses;
+
+import com.qualcomm.hardware.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by skaggsm on 11/14/15.
+ */
+public class MRGyroHelper {
+
+    public static final String TAG = "GYROHELPER";
+    private ModernRoboticsI2cGyro gyro;
+    private LinearOpMode linearOpMode;
+
+    public MRGyroHelper(ModernRoboticsI2cGyro gyro, LinearOpMode linearOpMode) {
+        this.gyro = gyro;
+        this.linearOpMode = linearOpMode;
+    }
+
+    public void calibrateAndWait() throws InterruptedException {
+        linearOpMode.telemetry.addData(TAG, "Starting calibration...");
+        gyro.calibrate();
+        while (gyro.isCalibrating()) {
+            linearOpMode.sleep(5);
+        }
+        linearOpMode.telemetry.addData(TAG, "Calibration complete!");
+    }
+
+    public int getIntegratedZValue() {
+        return gyro.getIntegratedZValue();
+    }
+}
