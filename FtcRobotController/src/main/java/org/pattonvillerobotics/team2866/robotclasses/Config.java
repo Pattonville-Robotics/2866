5f860ce4b4059f95a5791527a3802075f8a65820
magneticflux- <skaggsm333@gmail.com>
2015-11-17 17:01:01 -0600

    Tweaked rotation and encoder values
    Former-commit-id: 22cf1ffff15aec3286394d850aaa213845f7bcf7

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
@@ -27,6 +27,6 @@ public final class Config {
     public static final int ARM_MOVEMENT_SPEED = 1;
     public static final double CHAIN_MOVEMENT_SPEED = .25;
     public static final double LIFT_MOVEMENT_SPEED = .25;
-    public static final int ENCODER_MOVEMENT_TOLERANCE = 24;
-    public static final int GYRO_TURN_TOLERANCE = 2;
+    public static final int ENCODER_MOVEMENT_TOLERANCE = 12;
+    public static final int GYRO_TURN_TOLERANCE = 5;
 }
