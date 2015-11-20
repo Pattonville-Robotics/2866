ce105d109f11935ea8625b6a15c236cf3b6138f6
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-11-20 10:04:18 -0600

    Rename and addition of files for all autonomi

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoBucketMountainRed2.java
@@ -0,0 +1,18 @@
+package org.pattonvillerobotics.team2866.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.team2866.robotclasses.OpMode;
+
+/**
+ * Created by stewartk02 on 11/5/15.
+ *
+ * TODO: Measure and write OpMode
+ */
+public class AutoBucketMountainRed2 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
