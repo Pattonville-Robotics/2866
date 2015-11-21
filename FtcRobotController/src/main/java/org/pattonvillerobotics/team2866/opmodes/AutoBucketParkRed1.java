0c84b36c4eb308d784481c49c2b19e11579846bd
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-11-21 15:59:37 -0600

    Copy & reversed turns from Blue Park 1 into Red Park 1
    Former-commit-id: 6428e1681c80a0ee1a7712121ff4a51754fcca82

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoBucketParkRed1.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoBucketParkRed1.java
@@ -5,6 +5,10 @@ import android.widget.ListView;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.pattonvillerobotics.team2866.robotclasses.ClimbAssist;
+import org.pattonvillerobotics.team2866.robotclasses.ClimberDumper;
+import org.pattonvillerobotics.team2866.robotclasses.Direction;
+import org.pattonvillerobotics.team2866.robotclasses.Drive;
 import org.pattonvillerobotics.team2866.robotclasses.OpMode;
 
 /**
@@ -12,9 +16,25 @@ import org.pattonvillerobotics.team2866.robotclasses.OpMode;
  *
  * TODO: Measure and write OpMode
  */
+@OpMode("Red Park 1")
 public class AutoBucketParkRed1 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+
+        Drive drive = new Drive(hardwareMap, this);
+        ClimberDumper climberDumper = new ClimberDumper(hardwareMap);
+        ClimbAssist climbAssist = new ClimbAssist(hardwareMap);
+
+        waitForStart();
+
+        drive.moveInches(Direction.BACKWARDS, 30, 1); //Make sure this doesn't cross over! Go 30 inches
+        drive.rotateDegrees(Direction.LEFT, 45, 1); //Make sure this goes at a 45˚ angle!
+        drive.moveInches(Direction.BACKWARDS, 72, 1); //92 inches or w/e up to the rescue bit
+        drive.rotateDegrees(Direction.LEFT, 45, 1);
+        drive.moveInches(Direction.BACKWARDS, 18, 1); //18 inches: NIN*2
+        climberDumper.move(Direction.UP); //Moves the servo to dump the lil guys into the basket
+        climberDumper.move(Direction.DOWN);
+        drive.stop();
 
     }
 }
