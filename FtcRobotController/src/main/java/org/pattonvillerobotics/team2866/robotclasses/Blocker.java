a8e4924368133026c532e1b7eb00d13558731627
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-11-21 16:11:38 -0600

    Created template class for our new system, the Blocker

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Blocker.java
@@ -0,0 +1,32 @@
+package org.pattonvillerobotics.team2866.robotclasses;
+
+import android.widget.Button;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by skeltonn on 11/21/15.
+ * TODO: Find servo values
+ * TODO: Create a control method similar to ZipRelease
+ */
+public class Blocker {
+
+    private HardwareMap hardwareMap;
+
+    public Servo servoLeft;
+    public Servo servoRight;
+
+    private double upLeft = 0;
+    private double upRight = 0;
+    private double downLeft = 0;
+    private double downRight = 0;
+
+    public Blocker(HardwareMap hardwareMap) {
+
+        this.hardwareMap = hardwareMap;
+        servoLeft = hardwareMap.servo.get(Config.SERVO_BLOCKER_LEFT);
+        servoRight = hardwareMap.servo.get(Config.SERVO_BLOCKER_RIGHT);
+    }
+
+}
--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Config.java
@@ -24,10 +24,14 @@ public final class Config {
 
     public static final String SENSOR_GYRO = "sensor_gyro";
 
+    public static final String SERVO_BLOCKER_LEFT = "servo_blocker_left";
+    public static final String SERVO_BLOCKER_RIGHT = "servo_blocker_right";
+
     public static final int ARM_MOVEMENT_SPEED = 1;
     public static final double CHAIN_MOVEMENT_SPEED = .25;
     public static final double LIFT_MOVEMENT_SPEED = .25;
     public static final int ENCODER_MOVEMENT_TOLERANCE = 12;
     public static final int GYRO_TURN_TOLERANCE = 5;
     public static final int GYRO_TRIM = 5;
+
 }
