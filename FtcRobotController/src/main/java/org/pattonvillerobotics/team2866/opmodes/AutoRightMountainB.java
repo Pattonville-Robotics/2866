3e53a52e27f685a7b07ad6b6b6f25681927a5753
Kanye West For President <jpmcmahon115@icloud.com>
2015-11-24 15:24:07 -0600

    Changed AutoMountain and AutoPark values so that they align with the goals. Made AutoPark exist using code from AutoMountain basically spliced in half.
    Former-commit-id: d1d5a001a6f418c9053226a2b44246dc5a5bc15d

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoRightMountainB.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoRightMountainB.java
@@ -6,41 +6,34 @@ import org.pattonvillerobotics.team2866.robotclasses.ClimbAssist;
 import org.pattonvillerobotics.team2866.robotclasses.ClimberDumper;
 import org.pattonvillerobotics.team2866.robotclasses.Direction;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
-import org.pattonvillerobotics.team2866.robotclasses.OpMode;
-import org.pattonvillerobotics.team2866.robotclasses.ZipRelease;
 
 /**
- * Created by stewartk02 on 11/5/15.
- *
- * TODO: Measure and write OpMode
+ * Created by mcmahonj on 11/17/15.
  */
-@OpMode("Blue Mountain 2")
-public class AutoBucketMountainBlue2 extends LinearOpMode {
+public class AutoRightMountainB extends LinearOpMode {
+    public static final String TAG = "AutoLongMountainRed";
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         Drive drive = new Drive(hardwareMap, this);
         ClimberDumper climberDumper = new ClimberDumper(hardwareMap);
         ClimbAssist climbAssist = new ClimbAssist(hardwareMap);
-        ZipRelease zipRelease = new ZipRelease(hardwareMap);
-
-        zipRelease.moveLeft(Direction.DOWN);
-        zipRelease.moveRight(Direction.DOWN);
-        climberDumper.move(Direction.DOWN);
 
         waitForStart();
-
         drive.moveInches(Direction.BACKWARDS, 69, 1); //66
-        drive.rotateDegrees(Direction.RIGHT, 45, .5); //Make sure this goes at a 45˚ angle!
+
+        drive.rotateDegrees(Direction.LEFT, 45, 1); //Make sure this goes at a 45˚ angle!
         drive.moveInches(Direction.BACKWARDS, 24, 1); //92 inches or w/e up to the rescue bit
-        drive.rotateDegrees(Direction.RIGHT, 45, .5);
+        drive.rotateDegrees(Direction.LEFT, 45, 1);
         CommonAutonomous.dumpClimber(drive, climberDumper);
-        drive.rotateDegrees(Direction.LEFT, 45, .5);
+        drive.rotateDegrees(Direction.LEFT, 135, 1);
         drive.moveInches(Direction.FORWARDS, 12, 1);
-        drive.rotateDegrees(Direction.LEFT, 90, .4);
-        drive.moveInches(Direction.FORWARDS, 50, 1); //Measurement required
-        //climbAssist.moveChain(1);
-        //climbAssist.moveChain(0);
+        drive.rotateDegrees(Direction.RIGHT, 90, 1);
+        drive.moveInches(Direction.FORWARDS, 52, 1);
+        climbAssist.moveChain(1);
+        drive.moveInches(Direction.FORWARDS, 60, 1); //Measurement required
+        climbAssist.moveChain(0);
         drive.stop();
     }
 }
--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoRightMountainR.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/AutoRightMountainR.java
@@ -6,35 +6,36 @@ import org.pattonvillerobotics.team2866.robotclasses.ClimbAssist;
 import org.pattonvillerobotics.team2866.robotclasses.ClimberDumper;
 import org.pattonvillerobotics.team2866.robotclasses.Direction;
 import org.pattonvillerobotics.team2866.robotclasses.Drive;
-import org.pattonvillerobotics.team2866.robotclasses.OpMode;
-import org.pattonvillerobotics.team2866.robotclasses.ZipRelease;
 
 /**
- * Created by skeltonn on 11/20/15.
- *
- * TODO: Measure and write OpMode
+ * Created by mcmahonj on 11/12/15.
  */
-@OpMode("Blue Park 2")
-public class AutoBucketParkBlue2 extends LinearOpMode {
+public class AutoRightMountainR extends LinearOpMode {
+    public static final String TAG = "AutoLongMountainRed";
+
     @Override
     public void runOpMode() throws InterruptedException {
+
         Drive drive = new Drive(hardwareMap, this);
         ClimberDumper climberDumper = new ClimberDumper(hardwareMap);
         ClimbAssist climbAssist = new ClimbAssist(hardwareMap);
-        ZipRelease zipRelease = new ZipRelease(hardwareMap);
-
-        zipRelease.moveLeft(Direction.DOWN);
-        zipRelease.moveRight(Direction.DOWN);
-        climberDumper.move(Direction.DOWN);
 
         waitForStart();
-
         drive.moveInches(Direction.BACKWARDS, 69, 1); //66
-        drive.rotateDegrees(Direction.RIGHT, 45, .5); //Make sure this goes at a 45˚ angle!
+        drive.rotateDegrees(Direction.LEFT, 45, 1); //Make sure this goes at a 45˚ angle!
         drive.moveInches(Direction.BACKWARDS, 24, 1); //92 inches or w/e up to the rescue bit
-        drive.rotateDegrees(Direction.RIGHT, 45, .5);
+        drive.rotateDegrees(Direction.LEFT, 45, 1);
+        drive.moveInches(Direction.BACKWARDS, 18, 1); //18 inches: NIN*2
         climberDumper.move(Direction.UP); //Moves the servo to dump the lil guys into the basket
         climberDumper.move(Direction.DOWN);
+        drive.moveInches(Direction.FORWARDS, 18, 1);
+        drive.rotateDegrees(Direction.LEFT, 135, 1);
+        drive.moveInches(Direction.FORWARDS, 12, 1);
+        drive.rotateDegrees(Direction.RIGHT, 90, 1);
+        drive.moveInches(Direction.FORWARDS, 52, 1);
+        climbAssist.moveChain(1);
+        drive.moveInches(Direction.FORWARDS, 60, 1); //Measurement required
+        climbAssist.moveChain(0);
         drive.stop();
     }
 }
