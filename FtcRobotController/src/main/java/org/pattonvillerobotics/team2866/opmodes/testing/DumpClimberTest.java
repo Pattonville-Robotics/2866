317af7b6f96406f0318fc1eafab3c73cdee959fb
magneticflux- <skaggsm333@gmail.com>
2016-02-18 20:17:50 -0600

    Added climber dumper test and gyro driving toggle
    Former-commit-id: dc9248f9b57660d438ee7ac33965b6bb513838ef

---
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/opmodes/testing/DumpClimberTest.java
@@ -0,0 +1,26 @@
+package org.pattonvillerobotics.team2866.opmodes.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.team2866.opmodes.CommonAutonomous;
+import org.pattonvillerobotics.team2866.robotclasses.OpMode;
+import org.pattonvillerobotics.team2866.robotclasses.controllables.ClimberDumper;
+import org.pattonvillerobotics.team2866.robotclasses.controllables.Drive;
+
+/**
+ * Created by skaggsm on 2/18/16.
+ */
+@OpMode("DumpClimberTest")
+public class DumpClimberTest extends LinearOpMode {
+
+    public static final String TAG = DumpClimberTest.class.getSimpleName();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Drive drive = new Drive(this.hardwareMap, this);
+        ClimberDumper climberDumper = new ClimberDumper(this.hardwareMap);
+        waitForStart();
+
+        CommonAutonomous.smoothClimberMovement(drive, climberDumper);
+    }
+}
