a19babdc9ba3375b188426efeb34c987a58d4b17
Mitchell Skaggs <skaggsm333@gmail.com>
2016-03-12 07:35:54 -0600

    Gyro and encoder error readouts for debugging and analysis

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/controllables/DoubleGyroHelper.java
+++ FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/controllables/DoubleGyroHelper.java
@@ -10,39 +10,43 @@ import org.pattonvillerobotics.team2866.robotclasses.Config;
  */
 public class DoubleGyroHelper {
 
-    public final ModernRoboticsI2cGyro gyro1, gyro2;
+	public final ModernRoboticsI2cGyro gyro1, gyro2;
 
-    public DoubleGyroHelper(HardwareMap hardwareMap) throws InterruptedException {
-        gyro1 = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(Config.SENSOR_GYRO_1);
-        gyro2 = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(Config.SENSOR_GYRO_2);
+	public DoubleGyroHelper(HardwareMap hardwareMap) {
+		gyro1 = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(Config.SENSOR_GYRO_1);
+		gyro2 = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(Config.SENSOR_GYRO_2);
 
-        this.calibrate();
-    }
+		this.calibrate();
+	}
 
-    public void calibrate() throws InterruptedException {
-        gyro1.calibrate();
-        gyro2.calibrate();
-        while (gyro1.isCalibrating() || gyro2.isCalibrating()) {
-            Thread.yield();
-        }
-        Thread.sleep(3000);
-    }
+	public void calibrate() {
+		gyro1.calibrate();
+		gyro2.calibrate();
+		while (gyro1.isCalibrating() || gyro2.isCalibrating()) {
+			Thread.yield();
+		}
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public double getIntegratedZValue() {
-        return (gyro1.getIntegratedZValue() + gyro2.getIntegratedZValue()) / 2d;
-    }
+	public double getIntegratedZValue() {
+		return (gyro1.getIntegratedZValue() + gyro2.getIntegratedZValue()) / 2d;
+	}
 
-    public double getHeading() {
-        int heading1 = gyro1.getHeading();
-        int heading2 = gyro2.getHeading();
+	public double getHeading() {
+		int heading1 = gyro1.getHeading();
+		int heading2 = gyro2.getHeading();
 
-        if (Math.abs(heading1 - heading2) > 180) {
-            if (heading1 > 180)
-                heading1 -= 360;
-            if (heading2 > 180)
-                heading2 -= 360;
-        }
+		if (Math.abs(heading1 - heading2) > 180) {
+			if (heading1 > 180)
+				heading1 -= 360;
+			if (heading2 > 180)
+				heading2 -= 360;
+		}
 
-        return ((heading1 + heading2) / 2 + 360) % 360;
-    }
+		return (((heading1 + heading2) / 2d) + 360) % 360;
+	}
 }
