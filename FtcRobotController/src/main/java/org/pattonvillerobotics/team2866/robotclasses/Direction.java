a04051a4eab675b42f333adc5589a7c26efbc061
Mitchell Skaggs <skaggsm333@gmail.com>
2016-09-05 16:23:28 -0500

    Update repository
    Former-commit-id: b2f58826c28f624f0ed813d391703105aef1f30f

--- FtcRobotController/src/main/java/org/pattonvillerobotics/team2866/robotclasses/Direction.java
+++
@@ -1,8 +0,0 @@
-package org.pattonvillerobotics.team2866.robotclasses;
-
-/**
- * Created by skeltonn on 10/25/15.
- */
-public enum Direction {
-    UP, DOWN, LEFT, RIGHT, FORWARDS, BACKWARDS, MID
-}
