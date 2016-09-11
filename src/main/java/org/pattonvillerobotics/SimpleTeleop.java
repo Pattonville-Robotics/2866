d9a77e3e98ca90ec1ed53bd4cd4e29a77fa825fe
Mitchell Skaggs <skaggsm333@gmail.com>
2016-09-10 22:22:13 -0500

    Added missing files, fixed compilation errors, and removed AutoRegisteredOpMode annotation in favor of FTC's implementation You're welcome, Josh

---
+++ src/main/java/org/pattonvillerobotics/SimpleTeleop.java
@@ -0,0 +1,35 @@
+package org.pattonvillerobotics;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by skaggsm on 9/6/16.
+ */
+@TeleOp(name = "Simple Teleop", group = "Generic OpModes")
+public class SimpleTeleop extends LinearOpMode {
+    private static final String TAG = "SimpleTeleop";
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Log.e(TAG, "Starting");
+        DcMotor motorLeft = hardwareMap.dcMotor.get("motor_left");
+        DcMotor motorRight = hardwareMap.dcMotor.get("motor_right");
+        motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        this.waitForStart();
+
+        Log.e(TAG, "Starting loop");
+        while (this.opModeIsActive()) {
+            //Log.e(TAG,"Looping");
+            motorLeft.setPower(gamepad1.left_stick_y);
+            motorRight.setPower(gamepad1.right_stick_y);
+        }
+        Log.e(TAG, "Done");
+    }
+}
---
+++ src/main/java/org/pattonvillerobotics/TestAutonomous.java
@@ -0,0 +1,24 @@
+package org.pattonvillerobotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robot_classes.Drive;
+
+@Autonomous(name = "Simple Autonomous", group = "Generic OpModes")
+public class TestAutonomous extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Drive drive = new Drive(hardwareMap, this);
+
+        for (int i = 0; i < 4; i++) {
+            drive.move(Direction.FORWARD, .5);
+            sleep(1000);
+            drive.turn(Direction.LEFT, .5);
+            sleep(1000);
+        }
+    }
+}
