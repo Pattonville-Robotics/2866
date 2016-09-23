50f5110cbc638785de3455edaf17af579a9c636f
Mitchell Skaggs <skaggsm333@gmail.com>
2016-09-23 07:47:38 -0500

    Updated name of Drive to EncoderDrive

--- src/main/java/org/pattonvillerobotics/TestAutonomous.java
+++ src/main/java/org/pattonvillerobotics/TestAutonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.pattonvillerobotics.commoncode.enums.Direction;
-import org.pattonvillerobotics.commoncode.robotclasses.Drive;
+import org.pattonvillerobotics.commoncode.robotclasses.EncoderDrive;
 
 @Autonomous(name = "Simple Autonomous", group = "Generic OpModes")
 public class TestAutonomous extends LinearOpMode {
@@ -12,7 +12,7 @@ public class TestAutonomous extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        Drive drive = new Drive(hardwareMap, this);
+        EncoderDrive drive = new EncoderDrive(hardwareMap, this);
 
         for (int i = 0; i < 4; i++) {
             drive.move(Direction.FORWARD, .5);
