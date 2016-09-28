48792f2a6dcdbeb44559b1ff02772852b822f7eb
Mitchell Skaggs <skaggsm333@gmail.com>
2016-09-27 20:54:23 -0500

    Added default RobotParameters for 2866's robot (Not correct) and added calibration autonomous

---
+++ src/main/java/org/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
@@ -0,0 +1,21 @@
+package org.pattonvillerobotics.opmodes;
+
+import org.pattonvillerobotics.commoncode.robotclasses.RobotParameters;
+
+/**
+ * Created by skaggsm on 9/27/16.
+ */
+
+public class CustomizedRobotParameters {
+    public static final RobotParameters ROBOT_PARAMETERS;
+
+    static {
+        ROBOT_PARAMETERS = new RobotParameters.Builder()
+                .encodersEnabled(true)
+                .gyroEnabled(false)
+                .driveGearRatio(2)
+                .wheelBaseRadius(14)
+                .wheelRadius(3)
+                .build();
+    }
+}
