f6ac5200ca300e9afe3ced7b3b331a5026ed7dbe
tyarksc00 <colleentyarks@gmail.com>
2016-09-29 19:26:17 -0500

    Made autonomous that causes robot to go 5 inches forward
    Former-commit-id: d6e9729e02e18c15b69f6e06a6703371c9b6cb6c

---
+++ src/main/java/org/pattonvillerobotics/opmodes/ColleenAutonomous.java
@@ -0,0 +1,21 @@
+package org.pattonvillerobotics.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+
+/**
+ * Created by tyarksc00 on 9/29/16.
+ */
+@Autonomous(name = "colleen A")
+public class ColleenAutonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        AbstractComplexDrive drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+        waitForStart();
+        drive.moveInches(Direction.FORWARD, 5, .60);
+    }
+}
