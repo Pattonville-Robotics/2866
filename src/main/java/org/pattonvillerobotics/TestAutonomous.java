a7cd9a238f016338ef9c6b9838707bb6c6dbb7f3
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-13 19:53:02 -0500

    Updated to test gyro code

--- src/main/java/org/pattonvillerobotics/TestAutonomous.java
+++ src/main/java/org/pattonvillerobotics/TestAutonomous.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDrive;
-import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.GyroEncoderDrive;
 import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
 
 @Autonomous(name = "Testing Autonomous", group = "Generic OpModes")
@@ -13,11 +13,11 @@ public class TestAutonomous extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        AbstractComplexDrive drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+        AbstractComplexDrive drive = new GyroEncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
 
         waitForStart();
 
         //drive.moveInches(Direction.FORWARD, 100, 0.5);
-        drive.rotateDegrees(Direction.RIGHT, 360 * 4, .5);
+        drive.rotateDegrees(Direction.RIGHT, 360, .5);
     }
 }
