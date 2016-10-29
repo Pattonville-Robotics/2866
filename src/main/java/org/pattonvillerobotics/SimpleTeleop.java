4cc52d1d23e3234c634b4730e2ebbcfc8d83d3b5
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-29 10:25:32 -0500

    Updated SimpleTeleop

--- src/main/java/org/pattonvillerobotics/SimpleTeleop.java
+++ src/main/java/org/pattonvillerobotics/SimpleTeleop.java
@@ -4,31 +4,31 @@ import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
 
 /**
  * Created by skaggsm on 9/6/16.
  */
-@TeleOp(name = "Simple Teleop", group = "Generic OpM=odes")
+@TeleOp(name = "Simple Teleop", group = OpModeGroups.MAIN)
 public class SimpleTeleop extends LinearOpMode {
     private static final String TAG = "SimpleTeleop";
 
     @Override
     public void runOpMode() throws InterruptedException {
         Log.e(TAG, "Starting");
-        DcMotor motorLeft = hardwareMap.dcMotor.get("motor_left");
-        DcMotor motorRight = hardwareMap.dcMotor.get("motor_right");
-        motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        SimpleDrive drive = new SimpleDrive(this, hardwareMap);
+        ListenableGamepad listenableGamepad1 = new ListenableGamepad();
 
         this.waitForStart();
 
         Log.e(TAG, "Starting loop");
         while (this.opModeIsActive()) {
-            //Log.e(TAG,"Looping");
-            motorLeft.setPower(gamepad1.left_stick_y);
-            motorRight.setPower(gamepad1.right_stick_y);
+            listenableGamepad1.update(new GamepadData(gamepad1));
+            drive.moveFreely(gamepad1.left_stick_y, gamepad1.right_stick_y);
         }
         Log.e(TAG, "Done");
     }
