522e02d6d50606d117db2b8b5d898b68b7d7b157
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-29 00:09:55 -0500

    Began writing threads to update the Kalman Filter with data. Matrices are mostly written so far.

--- src/main/java/org/pattonvillerobotics/opmodes/KalmanFilterTest.java
+++ src/main/java/org/pattonvillerobotics/opmodes/KalmanFilterTest.java
@@ -1,38 +1,27 @@
 package org.pattonvillerobotics.opmodes;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.KalmanFilterGuidance;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by skaggsm on 10/25/16.
  */
 
+@Autonomous(name = "Kalman Filter Test", group = OpModeGroups.TESTING)
 public class KalmanFilterTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        final KalmanFilterGuidance kalmanFilterGuidance = new KalmanFilterGuidance();
         final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
-
-        Thread encoderThread = new Thread(new Runnable() {
-            long lastTimeNS = System.nanoTime();
-
-            @Override
-            public void run() {
-                while (opModeIsActive()) {
-                    long nowTimeNS = System.nanoTime();
-                    double elapsedTimeS = (nowTimeNS - lastTimeNS) / (double) TimeUnit.SECONDS.toNanos(1);
-
-                    //kalmanFilterGuidance.measure();
-                }
-            }
-        });
+        final ModernRoboticsI2cGyro gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        final KalmanFilterGuidance kalmanFilterGuidance = new KalmanFilterGuidance(this, encoderDrive, gyro);
 
         waitForStart();
 
-
+        kalmanFilterGuidance.run();
     }
 }
