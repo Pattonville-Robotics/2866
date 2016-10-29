9165e91ac5ca991a5f2a81316186f6c7df26a7d2
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-29 14:22:20 -0500

    Fixed gyro and encoder (rotation and straight line) motion integration and measurement and set good default covariance matrices

--- src/main/java/org/pattonvillerobotics/opmodes/KalmanFilterTest.java
+++ src/main/java/org/pattonvillerobotics/opmodes/KalmanFilterTest.java
@@ -1,9 +1,13 @@
 package org.pattonvillerobotics.opmodes;
 
+import android.util.Log;
+
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.apache.commons.math3.linear.RealVector;
+import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.KalmanFilterGuidance;
@@ -18,10 +22,41 @@ public class KalmanFilterTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
         final ModernRoboticsI2cGyro gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        gyro.calibrate();
+        while (gyro.isCalibrating())
+            idle();
         final KalmanFilterGuidance kalmanFilterGuidance = new KalmanFilterGuidance(this, encoderDrive, gyro);
+
+        telemetry.addData("Lifecycle", "Initialization Complete");
+        telemetry.update();
 
         waitForStart();
 
         kalmanFilterGuidance.run();
+
+        Thread printThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!isStopRequested()) {
+                    synchronized (kalmanFilterGuidance.kalmanFilter) {
+                        RealVector currentState = kalmanFilterGuidance.getCurrentState();
+                        Log.e("Current Angle: ", currentState.getEntry(6) + " degrees\nCurrent X: " + currentState.getEntry(0) + " in\nCurrent Y: " + currentState.getEntry(1) + " in");
+                    }
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        printThread.start();
+
+        encoderDrive.moveInches(Direction.FORWARD, 24, .2);
+        encoderDrive.rotateDegrees(Direction.RIGHT, 90, .2);
+        encoderDrive.moveInches(Direction.FORWARD, 24, .2);
+
+        kalmanFilterGuidance.stopThreads();
+        printThread.interrupt();
     }
 }
