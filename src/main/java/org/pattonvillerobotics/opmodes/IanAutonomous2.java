b7db869a3594626e2d87eed71d552bd7eecd76b3
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-30 11:54:02 -0500

    Fixed autonomous compilation
    Former-commit-id: a861ebd7104660926e81a287f747e0923eb9b850

--- src/main/java/org/pattonvillerobotics/opmodes/IanAutonomous2.java
+++ src/main/java/org/pattonvillerobotics/opmodes/IanAutonomous2.java
@@ -2,6 +2,7 @@ package org.pattonvillerobotics.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
@@ -10,10 +11,10 @@ import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
  * Created by mckeowni on 10/29/16.
  */
 
-@Autonomous (name = "Ian's Red Dumb Autonomous 2", group = "Generic OpModes")
-public class IanAutonomous extends LinearOpMode {
+@Autonomous(name = "Ian's Red Dumb Autonomous 2", group = "Generic OpModes")
+public class IanAutonomous2 extends LinearOpMode {
     @Override
-    public  void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException {
         AbstractComplexDrive drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
 
         waitForStart();
@@ -23,4 +24,5 @@ public class IanAutonomous extends LinearOpMode {
         drive.moveInches(Direction.FORWARD, 10, .2);
         drive.rotateDegrees(Direction.LEFT, 45, .2);
         drive.moveInches(Direction.FORWARD, 20, .2);
+    }
 }
