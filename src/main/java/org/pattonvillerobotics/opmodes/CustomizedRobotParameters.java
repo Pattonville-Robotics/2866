0e50f237f55dd8dd746258187378fb7628bc864e
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-12 12:30:56 -0600

    Made Vuforia navigation local to each beacon

--- src/main/java/org/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
+++ src/main/java/org/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
@@ -34,10 +34,16 @@ public class CustomizedRobotParameters {
                 .licenseKey("AclLpHb/////AAAAGa41kVT84EtWtYJZW0bIHf9DHg5EHVYWCqExQMx6bbuBtjFeYdvzZLExJiXnT31qDi3WI3QQnOXH8pLZ4cmb39d1w0Oi7aCwy35ODjMvG5qX+e2+3v0l3r1hPpM8P7KPTkRPIl+CGYEBvoNkVbGGjalCW7N9eFDV/T5CN/RQvZjonX/uBPKkEd8ciqK8vWgfy9aPEipAoyr997DDagnMQJ0ajpwKn/SAfaVPA4osBZ5euFf07/3IUnpLEMdMKfoIH6QYLVgwbPuVtUiJWM6flzWaAw5IIhy0XXWwI0nGXrzVjPwZlN3El4Su73ADK36qqOax/pNxD4oYBrlpfYiaFaX0Q+BNro09weXQEoz/Mfgm")
                 .cameraDirection(VuforiaLocalizer.CameraDirection.BACK)
                 .phoneLocation(0 * MM_PER_INCH, 0 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0)
+                /*
                 .addBeaconLocation(72 * MM_PER_INCH, -12 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, -90) //Wheels
                 .addBeaconLocation(36 * MM_PER_INCH, 72 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Tools
                 .addBeaconLocation(72 * MM_PER_INCH, 36 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, -90) //Legos
                 .addBeaconLocation(-12 * MM_PER_INCH, 72 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Gears
+                */
+                .addBeaconLocation(0 * MM_PER_INCH, 0 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Wheels
+                .addBeaconLocation(0 * MM_PER_INCH, 0 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Tools
+                .addBeaconLocation(0 * MM_PER_INCH, 0 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Legos
+                .addBeaconLocation(0 * MM_PER_INCH, 0 * MM_PER_INCH, 0 * MM_PER_INCH, AxesOrder.XYZ, 90, 0, 0) //Gears
                 .cameraMonitorViewId(R.id.cameraMonitorViewId)
                 .build();
     }
