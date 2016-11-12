c6a52f6fafa7e66799ffe1cfc9631375f0128d7f
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-12 13:36:46 -0600

    Began homing code to be used during autonomous and teleop
    Former-commit-id: 70ddfb805940e8f5d9f51957d1dfd834e653aeef

---
+++ src/main/java/org/pattonvillerobotics/opmodes/BeaconHomingUtil.java
@@ -0,0 +1,16 @@
+package org.pattonvillerobotics.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.vuforia.VuforiaNav;
+
+/**
+ * Created by skaggsm on 11/12/16.
+ */
+
+public class BeaconHomingUtil {
+    public static void homeToBeacon(AllianceColor allianceColor, EncoderDrive encoderDrive, LinearOpMode linearOpMode, VuforiaNav vuforiaNav) {
+    }
+}
