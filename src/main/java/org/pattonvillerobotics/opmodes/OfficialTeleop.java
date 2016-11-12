1a3681acd6c3ddbdf0e22c0511318c2ffdd2573e
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-12 10:39:11 -0600

    Added more telemetry and made telemetry update more often

--- src/main/java/org/pattonvillerobotics/opmodes/OfficialTeleop.java
+++ src/main/java/org/pattonvillerobotics/opmodes/OfficialTeleop.java
@@ -3,6 +3,7 @@ package org.pattonvillerobotics.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
@@ -23,13 +24,26 @@ public class OfficialTeleop extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         ListenableGamepad gamepad = new ListenableGamepad();
         SimpleDrive drive = new SimpleDrive(this, hardwareMap);
-        BeaconPresser beaconPresser = new BeaconPresser(hardwareMap);
+        final BeaconPresser beaconPresser = new BeaconPresser(hardwareMap);
+
+        telemetry.setMsTransmissionInterval(16);
+        final Telemetry.Item leftServo = telemetry.addData("Left Servo: ", "N/A").setRetained(true);
+        final Telemetry.Item rightServo = telemetry.addData("Right Servo: ", "N/A").setRetained(true);
+        final Telemetry.Item motorPowers = telemetry.addData("Motor Powers: ", "N/A").setRetained(true);
 
         gamepad.getButton(GamepadData.Button.X)
                 .addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
                     @Override
                     public void run() {
                         buttonXToggleOn = !buttonXToggleOn;
+
+                        if (buttonXToggleOn) {
+                            beaconPresser.setLeftServoPosition(0);
+                            leftServo.setValue("DOWN");
+                        } else {
+                            beaconPresser.setLeftServoPosition(1);
+                            leftServo.setValue("UP");
+                        }
                     }
                 });
         gamepad.getButton(GamepadData.Button.B)
@@ -37,30 +51,25 @@ public class OfficialTeleop extends LinearOpMode {
                     @Override
                     public void run() {
                         buttonBToggleOn = !buttonBToggleOn;
+
+                        if (buttonBToggleOn) {
+                            beaconPresser.setRightServoPosition(1);
+                            rightServo.setValue("DOWN");
+                        } else {
+                            beaconPresser.setRightServoPosition(0);
+                            rightServo.setValue("UP");
+                        }
                     }
                 });
 
         waitForStart();
 
         while (opModeIsActive()) {
-            drive.moveFreely(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+            drive.moveFreely(-gamepad1.right_stick_y, -gamepad1.left_stick_y);
 
-            if (buttonXToggleOn) {
-                beaconPresser.setLeftServoPosition(0);
-                telemetry.addData("Left Servo: ", "DOWN");
-            } else {
-                beaconPresser.setLeftServoPosition(1);
-                telemetry.addData("Left Servo: ", "UP");
-            }
-            if (buttonBToggleOn) {
-                beaconPresser.setRightServoPosition(1);
-                telemetry.addData("Right Servo: ", "UP");
-            } else {
-                beaconPresser.setRightServoPosition(0);
-                telemetry.addData("Right Servo: ", "DOWN");
-            }
+            motorPowers.setValue("Left power: " + (-gamepad1.right_stick_y) + " Right power: " + (-gamepad1.left_stick_y));
 
-
+            telemetry.update();
             gamepad.update(new GamepadData(gamepad1));
         }
     }
