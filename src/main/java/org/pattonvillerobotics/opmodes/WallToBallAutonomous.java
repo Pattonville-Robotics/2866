b3dd53e722a2c4ba2818d0d440c15e25d60baaaf
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-15 15:48:32 -0600

    Added ball hitting autonomi
    Former-commit-id: c4919ede915221fca57ee0186288d4eaa3dcbd5c

---
+++ src/main/java/org/pattonvillerobotics/opmodes/WallToBallAutonomous.java
@@ -0,0 +1,33 @@
+package org.pattonvillerobotics.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.robotclasses.CommonAutonomous;
+
+/**
+ * Created by skaggsm on 11/15/16.
+ */
+public final class WallToBallAutonomous {
+
+    @Autonomous(name = "RED Tile 1 to Ball", group = OpModeGroups.MAIN)
+    public static final class RedTile1 extends LinearOpMode {
+        @Override
+        public void runOpMode() throws InterruptedException {
+            final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+            CommonAutonomous.tile1ToBall(encoderDrive, this, AllianceColor.RED);
+        }
+    }
+
+    @Autonomous(name = "BLUE Tile 1 to Ball", group = OpModeGroups.MAIN)
+    public static final class BlueTile1 extends LinearOpMode {
+        @Override
+        public void runOpMode() throws InterruptedException {
+            final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+            CommonAutonomous.tile1ToBall(encoderDrive, this, AllianceColor.BLUE);
+        }
+    }
+}
