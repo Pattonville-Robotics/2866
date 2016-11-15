81a1ec3f7603b60d4fe9f888e5090f508f9de618
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-15 15:38:48 -0600

    Added tiles 2 and 3 to ball navigation

--- src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
+++ src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
@@ -2,6 +2,7 @@ package org.pattonvillerobotics.robotclasses;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.apache.commons.math3.util.FastMath;
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDrive;
@@ -11,6 +12,12 @@ import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDriv
  */
 
 public final class CommonAutonomous {
+
+    public static final double
+            WALL_TO_BALL_DISTANCE = 60,
+            TILE_SIZE = 22,
+            COS_45 = FastMath.cos(FastMath.toRadians(45));
+
     public static void tile1ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
         tile1ToBall(drive, linearOpMode, allianceColor, 0L);
     }
@@ -19,11 +26,62 @@ public final class CommonAutonomous {
         if (delayMS > 0)
             linearOpMode.sleep(delayMS);
 
+        drive.moveInches(Direction.FORWARD, WALL_TO_BALL_DISTANCE, .5);
+    }
+
+    public static void tile2ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
+        tile2ToBall(drive, linearOpMode, allianceColor, 0L);
+    }
+
+    public static void tile2ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor, long delayMS) {
+        if (delayMS > 0)
+            linearOpMode.sleep(delayMS);
+
+        Direction direction1 = null, direction2 = null;
+        final double diagonalDistance = TILE_SIZE * COS_45, straightDistance = 5;
+
         switch (allianceColor) {
             case RED:
+                direction1 = Direction.LEFT;
+                direction2 = Direction.RIGHT;
+                break;
             case BLUE:
-                drive.moveInches(Direction.FORWARD, 60, .5);
+                direction1 = Direction.RIGHT;
+                direction2 = Direction.LEFT;
                 break;
         }
+        drive.moveInches(Direction.FORWARD, straightDistance, .5);
+        drive.rotateDegrees(direction1, 45, .5);
+        drive.moveInches(Direction.FORWARD, diagonalDistance, .5);
+        drive.rotateDegrees(direction2, 45, .5);
+        drive.moveInches(Direction.FORWARD, WALL_TO_BALL_DISTANCE - straightDistance - diagonalDistance, .5);
+    }
+
+    public static void tile3ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
+        tile3ToBall(drive, linearOpMode, allianceColor, 0L);
+    }
+
+    public static void tile3ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor, long delayMS) {
+        if (delayMS > 0)
+            linearOpMode.sleep(delayMS);
+
+        Direction direction1 = null, direction2 = null;
+        final double diagonalDistance = TILE_SIZE * 2 * COS_45, straightDistance = 5;
+
+        switch (allianceColor) {
+            case RED:
+                direction1 = Direction.LEFT;
+                direction2 = Direction.RIGHT;
+                break;
+            case BLUE:
+                direction1 = Direction.RIGHT;
+                direction2 = Direction.LEFT;
+                break;
+        }
+        drive.moveInches(Direction.FORWARD, straightDistance, .5);
+        drive.rotateDegrees(direction1, 45, .5);
+        drive.moveInches(Direction.FORWARD, diagonalDistance, .5);
+        drive.rotateDegrees(direction2, 45, .5);
+        drive.moveInches(Direction.FORWARD, WALL_TO_BALL_DISTANCE - straightDistance - diagonalDistance, .5);
     }
 }
