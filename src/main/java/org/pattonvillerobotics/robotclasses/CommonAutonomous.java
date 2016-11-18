8b099b37c8920188afa323042699cf7ef4e3559f
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-18 10:07:13 -0600

    Added tile one to beacon 1 autonomous using camera and vuforia
    Former-commit-id: cbfe852c56ce065487f8e7c2f91135d5d2d46513

--- src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
+++ src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
@@ -5,8 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.apache.commons.math3.util.FastMath;
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
 import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robotclasses.colordetection.BeaconColorDetection;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractComplexDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.vuforia.VuforiaNav;
+import org.pattonvillerobotics.commoncode.vision.ftc.resq.Beacon;
 
 /**
  * Created by skaggsm on 11/15/16.
@@ -20,11 +22,11 @@ public final class CommonAutonomous {
             COS_45_I = 1 / FastMath.cos(FastMath.toRadians(45)),
             BACKUP_DISTANCE = 12;
 
-    public static void tile1ToBeacon1(VuforiaNav vuforiaNav, AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
-        tile1ToBeacon1(vuforiaNav, drive, linearOpMode, allianceColor, 0L);
+    public static void tile1ToBeacon1(VuforiaNav vuforiaNav, BeaconColorDetection beaconColorDetection, BeaconPresser beaconPresser, AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
+        tile1ToBeacon1(vuforiaNav, beaconColorDetection, beaconPresser, drive, linearOpMode, allianceColor, 0L);
     }
 
-    private static void tile1ToBeacon1(VuforiaNav vuforiaNav, AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor, long delayMS) {
+    private static void tile1ToBeacon1(VuforiaNav vuforiaNav, BeaconColorDetection beaconColorDetection, BeaconPresser beaconPresser, AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor, long delayMS) {
         if (delayMS > 0)
             linearOpMode.sleep(delayMS);
 
@@ -42,13 +44,40 @@ public final class CommonAutonomous {
                 break;
         }
 
-        vuforiaNav.getNearestBeaconLocation();
-        float[] location = vuforiaNav.getLocation();
+        drive.moveInches(Direction.FORWARD, 6, .5);
+        drive.rotateDegrees(direction1, 45, 0.25);
+        drive.moveInches(Direction.FORWARD, 56, .5);
+        drive.rotateDegrees(direction2, 45, 0.25);
+        drive.moveInches(Direction.FORWARD, 10, .5);
 
-        while (linearOpMode.opModeIsActive() && location[0] / VuforiaNav.MM_PER_INCH > 2) {
+        float[] location;
+        final double approachSpeed = .25;
+
+        beaconColorDetection.setAnalysisMethod(Beacon.AnalysisMethod.COMPLEX);
+        Beacon.BeaconAnalysis beaconAnalysis = beaconColorDetection.analyzeFrame(vuforiaNav.getImage());
+
+        if ((beaconAnalysis.isLeftRed() && allianceColor == AllianceColor.RED) ||
+                (beaconAnalysis.isLeftBlue() && allianceColor == AllianceColor.BLUE))
+            beaconPresser.setLeftServoDown();
+
+        if ((beaconAnalysis.isRightRed() && allianceColor == AllianceColor.RED) ||
+                (beaconAnalysis.isRightBlue() && allianceColor == AllianceColor.BLUE))
+            beaconPresser.setRightServoDown();
+
+        while (linearOpMode.opModeIsActive()) {
             vuforiaNav.getNearestBeaconLocation();
             location = vuforiaNav.getLocation();
+
+            double distanceX = location[0] / VuforiaNav.MM_PER_INCH, distanceY = location[1] / VuforiaNav.MM_PER_INCH;
+
+            if (distanceX < 6)
+                break;
+
+            double multiplier = distanceX * (distanceY / 400);
+
+            drive.moveFreely(approachSpeed * (1 + multiplier), approachSpeed * (1 - multiplier));
         }
+        drive.stop();
     }
 
     public static void tile1ToBall(AbstractComplexDrive drive, LinearOpMode linearOpMode, AllianceColor allianceColor) {
