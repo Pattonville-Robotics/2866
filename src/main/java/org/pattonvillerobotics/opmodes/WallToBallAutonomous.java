e608c5b16e94c7db786eef8b6901648c35d20d4d
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-19 07:55:02 -0600

    Added tile 3 to ball autonomous
    Former-commit-id: 46b67e0834e8af8d5a6b7d97888c47db4bf4ed34

--- src/main/java/org/pattonvillerobotics/opmodes/WallToBallAutonomous.java
+++ src/main/java/org/pattonvillerobotics/opmodes/WallToBallAutonomous.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
 import org.pattonvillerobotics.robotclasses.CommonAutonomous;
@@ -12,6 +13,28 @@ import org.pattonvillerobotics.robotclasses.CommonAutonomous;
  * Created by skaggsm on 11/15/16.
  */
 public final class WallToBallAutonomous {
+
+    @Autonomous(name = "RED Tile 3 to Ball", group = OpModeGroups.MAIN)
+    public static final class RedTile3 extends LinearOpMode {
+        @Override
+        public void runOpMode() throws InterruptedException {
+            final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+            waitForStart();
+            //CommonAutonomous.tile1ToBall(encoderDrive, this, AllianceColor.RED);
+            encoderDrive.moveInches(Direction.FORWARD, 60, .75);
+        }
+    }
+
+    @Autonomous(name = "BLUE Tile 3 to Ball", group = OpModeGroups.MAIN)
+    public static final class BlueTile3 extends LinearOpMode {
+        @Override
+        public void runOpMode() throws InterruptedException {
+            final EncoderDrive encoderDrive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+            waitForStart();
+            //CommonAutonomous.tile1ToBall(encoderDrive, this, AllianceColor.RED);
+            encoderDrive.moveInches(Direction.FORWARD, 60, .75);
+        }
+    }
 
     @Autonomous(name = "RED Tile 1 to Ball", group = OpModeGroups.MAIN)
     public static final class RedTile1 extends LinearOpMode {
