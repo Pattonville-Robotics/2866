24ab70709c156f0ce9b3f6e5980e6750862152dc
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-30 11:36:19 -0600

    Added toggle for Vuforia telemetry

--- src/main/java/org/pattonvillerobotics/opmodes/OfficialTeleop.java
+++ src/main/java/org/pattonvillerobotics/opmodes/OfficialTeleop.java
@@ -21,8 +21,9 @@ import java.util.Arrays;
  * Created by skaggsm on 11/10/16.
  */
 
-@TeleOp(name = "Official Teleop", group = OpModeGroups.MAIN)
 public class OfficialTeleop extends LinearOpMode {
+
+    protected boolean vuforiaEnabled = false;
     private boolean buttonXToggleOn = false, buttonBToggleOn = false;
 
     @Override
@@ -31,17 +32,26 @@ public class OfficialTeleop extends LinearOpMode {
         ListenableGamepad gamepad = new ListenableGamepad();
         EncoderDrive drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
         final BeaconPresser beaconPresser = new BeaconPresser(hardwareMap);
-        VuforiaNav vuforiaNav = new VuforiaNav(CustomizedRobotParameters.VUFORIA_PARAMETERS);
-        BeaconColorDetection beaconColorDetection = new BeaconColorDetection(hardwareMap);
-        beaconColorDetection.setAnalysisMethod(Beacon.AnalysisMethod.REALTIME);
+
+        VuforiaNav vuforiaNav = null;
+        BeaconColorDetection beaconColorDetection = null;
+        if (vuforiaEnabled) {
+            vuforiaNav = new VuforiaNav(CustomizedRobotParameters.VUFORIA_PARAMETERS);
+            beaconColorDetection = new BeaconColorDetection(hardwareMap);
+            beaconColorDetection.setAnalysisMethod(Beacon.AnalysisMethod.REALTIME);
+        }
 
         telemetry.setMsTransmissionInterval(33);
         final Telemetry.Item leftServo = telemetry.addData("Left Servo: ", "N/A").setRetained(true);
         final Telemetry.Item rightServo = telemetry.addData("Right Servo: ", "N/A").setRetained(true);
         final Telemetry.Item leftMotorPower = telemetry.addData("Left Motor Power: ", "N/A").setRetained(true);
         final Telemetry.Item rightMotorPower = telemetry.addData("Right Motor Power: ", "N/A").setRetained(true);
-        final Telemetry.Item vuforiaLocation = telemetry.addData("Vuforia Location: ", "N/A").setRetained(true);
-        final Telemetry.Item beaconColors = telemetry.addData("Observed Beacon State: ", "N/A").setRetained(true);
+        Telemetry.Item vuforiaLocation = null;
+        Telemetry.Item beaconColors = null;
+        if (vuforiaEnabled) {
+            vuforiaLocation = telemetry.addData("Vuforia Location: ", "N/A").setRetained(true);
+            beaconColors = telemetry.addData("Observed Beacon State: ", "N/A").setRetained(true);
+        }
 
         gamepad.getButton(GamepadData.Button.X)
                 .addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
@@ -82,14 +92,30 @@ public class OfficialTeleop extends LinearOpMode {
             leftMotorPower.setValue(-gamepad1.left_stick_y);
             rightMotorPower.setValue(-gamepad1.right_stick_y);
 
-            vuforiaNav.getNearestBeaconLocation();
-            float[] location = vuforiaNav.getLocation();
-            vuforiaLocation.setValue(Arrays.toString(location));
-            Beacon.BeaconAnalysis beaconAnalysis = beaconColorDetection.analyzeFrame(vuforiaNav.getImage(), ScreenOrientation.PORTRAIT_REVERSE);
-            beaconColors.setValue(beaconAnalysis.toString());
+            if (vuforiaNav != null && vuforiaLocation != null) {
+                vuforiaNav.getNearestBeaconLocation();
+                float[] location = vuforiaNav.getLocation();
+                vuforiaLocation.setValue(Arrays.toString(location));
+                Beacon.BeaconAnalysis beaconAnalysis = beaconColorDetection.analyzeFrame(vuforiaNav.getImage(), ScreenOrientation.PORTRAIT_REVERSE);
+                beaconColors.setValue(beaconAnalysis.toString());
+            }
 
             telemetry.update();
             gamepad.update(new GamepadData(gamepad1));
+        }
+    }
+
+    @TeleOp(name = "Official Teleop", group = OpModeGroups.MAIN)
+    public static final class FastStart extends OfficialTeleop {
+        {
+            this.vuforiaEnabled = false;
+        }
+    }
+
+    @TeleOp(name = "Official Teleop (Vuforia Telemetry)", group = OpModeGroups.TESTING)
+    public static final class VuforiaTelemetry extends OfficialTeleop {
+        {
+            this.vuforiaEnabled = true;
         }
     }
 }
