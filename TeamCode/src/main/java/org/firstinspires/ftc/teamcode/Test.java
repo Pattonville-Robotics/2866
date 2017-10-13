413b8805f430f1422fae45da114b0dc445984414
Mitchell Skaggs <skaggsm333@gmail.com>
2017-10-13 08:15:32 -0500

    Replaced repo with v3.4 of ftc_app, replaced libs with Maven coordinates, added Common-Code dependency, updated Gradle Wrapper to 4.2.1
    Former-commit-id: 87aceb48a16d24a877ad359d4c557c0f93f683db

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.pattonvillerobotics.commoncode.enums.Direction;
+
+/**
+ * Created by Mitchell on 10/12/2017.
+ */
+
+public class Test {
+    Direction direction = Direction.LEFT;
+}
